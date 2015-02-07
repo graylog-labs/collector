270d7f540c128d07376708dafb83bb532be14fa5
Bernd Ahlers <bernd@graylog.com>
2015-02-07 17:42:42 +0100

    Catch cli parsing exception.

--- src/main/java/com/graylog/agent/cli/Main.java
+++ src/main/java/com/graylog/agent/cli/Main.java
@@ -4,8 +4,13 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.graylog.agent.cli.commands.Server;
 import io.airlift.airline.Cli;
+import io.airlift.airline.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         final Injector injector = Guice.createInjector();
 
@@ -15,6 +20,11 @@ public class Main {
 
         final Cli<Runnable> cli = cliBuilder.build();
 
-        cli.parse(args).run();
+        try {
+            cli.parse(args).run();
+        } catch (ParseException e) {
+            LOG.error(e.getMessage());
+            LOG.error("Exit");
+        }
     }
 }
