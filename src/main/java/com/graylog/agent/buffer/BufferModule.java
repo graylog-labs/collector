98b38dcc5b1ae798c2dce07bfbe128b2839c1e65
Bernd Ahlers <bernd@graylog.com>
2015-02-09 14:23:12 +0100

    Make sure Buffer is a singleton.

--- src/main/java/com/graylog/agent/buffer/BufferModule.java
+++ src/main/java/com/graylog/agent/buffer/BufferModule.java
@@ -1,12 +1,13 @@
 package com.graylog.agent.buffer;
 
+import com.google.inject.Scopes;
 import com.graylog.agent.guice.AgentModule;
 
 public class BufferModule extends AgentModule {
     @Override
     protected void configure() {
         bind(MessageBufferConfiguration.class);
-        bind(Buffer.class).to(MessageBuffer.class);
+        bind(Buffer.class).to(MessageBuffer.class).in(Scopes.SINGLETON);
 
         registerService(BufferProcessor.class);
     }
