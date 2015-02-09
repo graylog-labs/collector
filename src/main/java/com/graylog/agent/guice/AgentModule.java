afa7237aa3d5ea8e677795fff30f8086ba60e05c
Bernd Ahlers <bernd@graylog.com>
2015-02-09 10:26:06 +0100

    Move some code to GuiceUtils.

--- src/main/java/com/graylog/agent/guice/AgentModule.java
+++ src/main/java/com/graylog/agent/guice/AgentModule.java
@@ -17,9 +17,6 @@ import com.graylog.agent.outputs.OutputConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nullable;
-import java.lang.annotation.Annotation;
-
 public abstract class AgentModule extends AbstractModule {
     private static final Logger LOG = LoggerFactory.getLogger(AgentModule.class);
 
@@ -32,7 +29,7 @@ public abstract class AgentModule extends AbstractModule {
         @SuppressWarnings("unchecked")
         final Class<? extends Input.Factory<? extends Input, ? extends InputConfiguration>> factoryClass =
                 (Class<? extends Input.Factory<? extends Input, ? extends InputConfiguration>>)
-                        findInnerClassAnnotatedWith(AgentInputFactory.class, inputClass, Input.Factory.class);
+                        GuiceUtils.findInnerClassAnnotatedWith(AgentInputFactory.class, inputClass, Input.Factory.class);
 
         if (factoryClass != null) {
             install(new FactoryModuleBuilder().implement(Input.class, inputClass).build(factoryClass));
@@ -45,7 +42,7 @@ public abstract class AgentModule extends AbstractModule {
         @SuppressWarnings("unchecked")
         final Class<? extends Output.Factory<? extends Output, ? extends OutputConfiguration>> factoryClass =
                 (Class<? extends Output.Factory<? extends Output, ? extends OutputConfiguration>>)
-                        findInnerClassAnnotatedWith(AgentOutputFactory.class, outputClass, Output.Factory.class);
+                        GuiceUtils.findInnerClassAnnotatedWith(AgentOutputFactory.class, outputClass, Output.Factory.class);
 
         if (factoryClass != null) {
             install(new FactoryModuleBuilder().implement(Output.class, outputClass).build(factoryClass));
@@ -72,7 +69,7 @@ public abstract class AgentModule extends AbstractModule {
         @SuppressWarnings("unchecked")
         final Class<? extends InputConfiguration.Factory<? extends InputConfiguration>> factoryClass =
                 (Class<? extends InputConfiguration.Factory<? extends InputConfiguration>>)
-                        findInnerClassAnnotatedWith(AgentConfigurationFactory.class, configClass, InputConfiguration.Factory.class);
+                        GuiceUtils.findInnerClassAnnotatedWith(AgentConfigurationFactory.class, configClass, InputConfiguration.Factory.class);
 
         if (factoryClass == null) {
             LOG.error("No configuration factory found for {}. Make sure to create an inner Factory interface annotated with @{}.",
@@ -105,7 +102,7 @@ public abstract class AgentModule extends AbstractModule {
         @SuppressWarnings("unchecked")
         final Class<? extends OutputConfiguration.Factory<? extends OutputConfiguration>> factoryClass =
                 (Class<? extends OutputConfiguration.Factory<? extends OutputConfiguration>>)
-                        findInnerClassAnnotatedWith(AgentConfigurationFactory.class, configClass, OutputConfiguration.Factory.class);
+                        GuiceUtils.findInnerClassAnnotatedWith(AgentConfigurationFactory.class, configClass, OutputConfiguration.Factory.class);
 
         if (factoryClass == null) {
             LOG.error("No configuration factory found for {}. Make sure to create an inner Factory interface annotated with @{}.",
@@ -116,30 +113,5 @@ public abstract class AgentModule extends AbstractModule {
         install(new FactoryModuleBuilder().implement(Configuration.class, configClass).build(factoryClass));
 
         outputsMapBinder.addBinding(type).to(factoryClass);
-    }
-
-    @Nullable
-    protected Class<?> findInnerClassAnnotatedWith(Class<? extends Annotation> annotationClass,
-                                                   Class<?> containingClass,
-                                                   Class<?> targetClass) {
-        final Class<?>[] declaredClasses = containingClass.getDeclaredClasses();
-        Class<?> annotatedClass = null;
-        for (final Class<?> declaredClass : declaredClasses) {
-            if (!declaredClass.isAnnotationPresent(annotationClass)) {
-                continue;
-            }
-            if (targetClass.isAssignableFrom(declaredClass)) {
-                if (annotatedClass != null) {
-                    LOG.error("Multiple annotations for {} found in {}. This is invalid.", annotatedClass.getSimpleName(), containingClass);
-                    return null;
-                }
-                annotatedClass = declaredClass;
-            } else {
-                LOG.error("{} annotated as {} is not extending the expected {}. Did you forget to implement the correct interface?",
-                        declaredClass, annotationClass.getSimpleName(), targetClass);
-                return null;
-            }
-        }
-        return annotatedClass;
     }
 }
