c927a2c78d4d6a6ca63a1cf40f9505ba03380b4b
Bernd Ahlers <bernd@graylog.com>
2015-02-10 19:46:50 +0100

    Start instrumenting the MessageBuffer.

--- src/main/java/com/graylog/agent/buffer/MessageBuffer.java
+++ src/main/java/com/graylog/agent/buffer/MessageBuffer.java
@@ -1,5 +1,7 @@
 package com.graylog.agent.buffer;
 
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.Queues;
 import com.graylog.agent.Message;
 import org.slf4j.Logger;
@@ -8,14 +10,23 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import java.util.concurrent.BlockingQueue;
 
+import static com.codahale.metrics.MetricRegistry.name;
+
 public class MessageBuffer implements Buffer {
     private static final Logger LOG = LoggerFactory.getLogger(MessageBuffer.class);
 
     private final BlockingQueue<Message> queue;
+    private final MetricRegistry metricRegistry;
+    private final Meter inserted;
+    private final Meter removed;
 
     @Inject
-    public MessageBuffer(MessageBufferConfiguration config) {
+    public MessageBuffer(MessageBufferConfiguration config, MetricRegistry metricRegistry) {
+        this.metricRegistry = metricRegistry;
         this.queue = Queues.newLinkedBlockingQueue(config.getSize());
+
+        this.inserted = metricRegistry.meter(name(getClass(), "inserted"));
+        this.removed = metricRegistry.meter(name(getClass(), "removed"));
     }
 
     public void insert(Message message) {
@@ -23,6 +34,7 @@ public class MessageBuffer implements Buffer {
 
         try {
             queue.put(message);
+            inserted.mark();
         } catch (InterruptedException e) {
             LOG.error("Interrupted, dropping message.", e);
         }
@@ -31,7 +43,13 @@ public class MessageBuffer implements Buffer {
     @Override
     public Message remove() {
         try {
-            return queue.take();
+            final Message message = queue.take();
+
+            if (message != null) {
+                removed.mark();
+            }
+
+            return message;
         } catch (InterruptedException e) {
             LOG.error("Interrupted while removing from buffer.", e);
             return null;
