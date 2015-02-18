f532808cc3bc653a9256cb690ea60de226c1d987
Bernd Ahlers <bernd@graylog.com>
2015-02-18 22:00:42 +0100

    Add version command.
    Fixes #2.

--- src/main/java/com/graylog/agent/cli/Main.java
+++ src/main/java/com/graylog/agent/cli/Main.java
@@ -3,6 +3,7 @@ package com.graylog.agent.cli;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.graylog.agent.cli.commands.Server;
+import com.graylog.agent.cli.commands.Version;
 import io.airlift.airline.Cli;
 import io.airlift.airline.ParseException;
 import org.slf4j.Logger;
@@ -16,6 +17,7 @@ public class Main {
 
         final Cli.CliBuilder<Runnable> cliBuilder = Cli.<Runnable>builder("graylog-agent")
                 .withDescription("Graylog agent")
+                .withCommand(Version.class)
                 .withCommand(Server.class);
 
         final Cli<Runnable> cli = cliBuilder.build();
---
+++ src/main/java/com/graylog/agent/cli/commands/Version.java
@@ -0,0 +1,16 @@
+package com.graylog.agent.cli.commands;
+
+import com.graylog.agent.AgentVersion;
+import io.airlift.airline.Command;
+
+@Command(name = "version", description = "Show version information on STDOUT")
+public class Version implements Runnable {
+    @Override
+    public void run() {
+        final AgentVersion v = AgentVersion.CURRENT;
+        final String message = String.format("Graylog Agent v%s (commit=%s, timestamp=%s)",
+                v.version(), v.commitIdShort(), v.timestamp());
+
+        System.out.println(message);
+    }
+}
