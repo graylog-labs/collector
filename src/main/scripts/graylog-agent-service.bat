f775163b1727c43f9d439175789a066678948c03
Bernd Ahlers <bernd@graylog.com>
2015-04-27 18:14:51 +0200

    Add windows scripts and binaries to install the agent as service.
    The binaries have been taken from the Apache commons-daemon-1.0.15
    release.
    
    Also make sure to package all scripts and binaries in the assembly.

---
+++ src/main/scripts/graylog-agent-service.bat
@@ -0,0 +1,100 @@
+:: Graylog Agent service management for Windows.
+
+@ECHO OFF
+
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+TITLE Graylog Agent Service ${project.version}
+
+:: Check if JAVA_HOME environment variable is set.
+IF DEFINED JAVA_HOME goto :dispatchAction
+
+:: Exit if there is no JAVA_HOME set.
+ECHO JAVA_HOME not set! 1>&2
+EXIT /B 1
+
+:dispatchAction
+:: Get the ACTION and SERVICE_NAME from the script arguments.
+IF "x%1x" == "xx" GOTO usage
+SET ACTION=%1
+SHIFT
+IF "x%1x" == "xx" GOTO usage
+SET SERVICE_NAME=%1
+
+:: Get the directory of this script.
+SET AGENT_BIN_DIR=%~dp0
+
+:: Get root directory of the Agent.
+FOR %%D in ("%AGENT_BIN_DIR%..") DO SET AGENT_ROOT=%%~dpfD
+
+:: Detect if we are running on 32bit or 64bit Windows.
+IF "%PROCESSOR_ARCHITECTURE%"=="x64" (SET ARCH=x64) ELSE (SET ARCH=x86)
+
+:: Use the correct executable based on the architecture.
+SET PROCRUN="%AGENT_BIN_DIR%\windows\graylog-agent-prunsrv-%ARCH%.exe"
+
+:: Dispatch the supported commands, show usage otherwise.
+IF /i %ACTION% == install GOTO actionInstallCheck
+IF /i %ACTION% == uninstall GOTO actionUninstall
+
+:usage
+ECHO.
+ECHO Usage: %~nx0 install^|uninstall^|start^|stop SERVICE_NAME
+GOTO:EOF
+
+:actionInstallCheck
+ECHO Installing service for Graylog Agent %AGENT_VERSION%
+ECHO.
+ECHO Service name: "%SERVICE_NAME%"
+ECHO JAVA_HOME:    "%JAVA_HOME%"
+ECHO ARCH:         "%ARCH%"
+
+:: Select the correct JVM DLL.
+SET JVM_DLL=%JAVA_HOME%\jre\bin\server\jvm.dll
+IF EXIST "%JVM_DLL%" GOTO actionInstall
+SET JVM_DLL=%JAVA_HOME%\bin\server\jvm.dll
+IF EXIST "%JVM_DLL%" GOTO actionInstall
+SET JVM_DLL=%JAVA_HOME%\bin\client\jvm.dll
+
+IF EXIST "%JVM_DLL%" (
+ECHO WARNING: JAVA_HOME points to a JRE and not JDK installation; a client (not a server^) JVM will be used...
+) ELSE (
+ECHO ERROR: Invalid Java installation (no jvm.dll found in "%JAVA_HOME%"^). Abort...
+GOTO:EOF
+)
+
+:actionInstall
+SET AGENT_VERSION=${project.version}
+SET AGENT_JAR="%AGENT_ROOT%\${project.artifactId}.jar"
+SET AGENT_CLASS=com.graylog.agent.cli.Main
+SET AGENT_JVM_MS=12m
+SET AGENT_JVM_MX=64m
+SET AGENT_STOP_TIMEOUT=0
+SET AGENT_STARTUP=auto
+SET AGENT_MODE=jvm
+SET AGENT_START_METHOD=main
+SET AGENT_START_PARAMS="server;-f;%AGENT_ROOT%\config\agent.conf"
+SET AGENT_STOP_METHOD=stop
+SET AGENT_PID_FILE="%SERVICE_NAME%.pid"
+SET AGENT_LOG_DIR="%AGENT_ROOT%\logs"
+SET AGENT_LOG_OPTIONS=--LogPath "%AGENT_LOG_DIR%" --LogPrefix "graylog-agent" --StdError auto --StdOutput auto
+
+"%PROCRUN%" //IS//%SERVICE_NAME% --Classpath "%AGENT_JAR%" --Jvm "%JVM_DLL%" --JvmMs %AGENT_JVM_MS% --JvmMx %AGENT_JVM_MX% --StartPath "%AGENT_ROOT%" --Startup %AGENT_STARTUP% --StartMode %AGENT_MODE% --StartClass %AGENT_CLASS% --StartMethod %AGENT_START_METHOD% --StartParams %AGENT_START_PARAMS% --StopMode %AGENT_MODE% --StopClass %AGENT_CLASS% --StopMethod %AGENT_STOP_METHOD% --StopTimeout %AGENT_STOP_TIMEOUT% --PidFile "%AGENT_PID_FILE%" --DisplayName "Graylog Agent (%SERVICE_NAME)" --Description "Graylog Agent %AGENT_VERSION% service. See http://www.graylog.org/ for details." %AGENT_LOG_OPTIONS%
+
+IF NOT errorlevel 1 GOTO actionInstallSuccess
+ECHO ERROR: Failed to install service: %SERVICE_NAME%
+GOTO:EOF
+
+:actionInstallSuccess
+ECHO Service '%SERVICE_NAME%' has been installed
+GOTO:EOF
+
+:actionUninstall
+:: Remove the service
+"%PROCRUN%" //DS//%SERVICE_NAME% %AGENT_LOG_OPTIONS%
+IF NOT errorlevel 1 GOTO actionUninstallSuccess
+ECHO ERROR: Failed to uninstall service: %SERVICE_NAME%
+GOTO:EOF
+
+:actionUninstallSuccess
+ECHO Service '%SERVICE_NAME%' has been removed
+GOTO:EOF
