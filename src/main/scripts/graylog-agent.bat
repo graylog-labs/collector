a5a98f960abf4230edc58dbcbc2c81008ad0532c
Bernd Ahlers <bernd@graylog.com>
2015-04-27 12:44:28 +0200

    Make windows batch script actually work.

--- src/main/scripts/graylog-agent.bat
+++ src/main/scripts/graylog-agent.bat
@@ -5,17 +5,21 @@
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 TITLE Graylog Agent ${project.version}
 
-if NOT DEFINED JAVA_HOME goto :jvmError
+IF DEFINED JAVA_HOME goto :continue
 
+:: JAVA_HOME needs to be set to find the JVM.
 :jvmError
 ECHO JAVA_HOME not set! 1>&2
 EXIT /B 1
 
+:: Execute the JAR.
 :continue
 set BIN_DIR=%~dp0
+
+:: Get root directory of the Agent.
 FOR %%D in ("%BIN_DIR%..") DO SET AGENT_ROOT=%%~dpfD
 
-SET AGENT_JAR="%AGENT_ROOT%\${project.artifactId}"
+SET AGENT_JAR="%AGENT_ROOT%\${project.artifactId}.jar"
 
 "%JAVA_HOME%\bin\java" ${agent.jvm-opts} -jar "%AGENT_JAR%" %*
 
